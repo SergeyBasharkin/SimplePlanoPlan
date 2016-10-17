da69330916ebac68ab53956b2393d36dff7e1903
SergeyBasharkin <rock-n-rolllll@bk.ru>
2016-10-17 20:51:24 +0300

    final commit

---
+++ src/js/mouseCoords.js
@@ -0,0 +1,23 @@
+/**
+ * ?????? ???????? ? ???????????? ???????
+ */
+var MouseCoords = {
+    // X-??????????
+    getX: function (e) {
+        if (e.pageX) {
+            return e.pageX;
+        } else if (e.clientX) {
+            return e.clientX + (document.documentElement.scrollLeft || document.body.scrollLeft) - document.documentElement.clientLeft;
+        }
+        return 0;
+    },
+    // Y-??????????
+    getY: function (e) {
+        if (e.pageY) {
+            return e.pageY;
+        } else if (e.clientY) {
+            return e.clientY + (document.documentElement.scrollTop || document.body.scrollTop) - document.documentElement.clientTop;
+        }
+        return 0;
+    }
+};
